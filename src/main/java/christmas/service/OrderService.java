f47583e4d7b7612d2cae7420e851f78630b084cb
inpink <inpink@kakao.com>
2023-12-12 18:13:35 +0900

    refactor: 할인 계산 분리

--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -1,4 +1,12 @@
 package christmas.service;
 
+import christmas.domain.entity.DateOfVisit;
+import christmas.domain.entity.OrderItems;
+import christmas.domain.entity.event.Benefit;
+import christmas.domain.entity.event.Events;
+
 public class OrderService {
+    public Benefit generateBenefit(DateOfVisit date, OrderItems orderItems) {
+        return Events.calculateBenefit(date, orderItems);
+    }
 }
